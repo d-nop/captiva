f39d9b35cdddcad6e61dc4fb311456cdae281b72
Steven <stevenesammon@hotmail.com>
2018-02-05 18:23:11 -0500

    adds dependencies and basic API routes

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const logger = require("morgan");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const db = require("./models");
 
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
+app.use(logger("dev"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.Promise = Promise;
+const mongoDB_URI = process.env.MONGODB_URI || "mongodb://localhost/#";
+mongoose.connect(mongoDB_URI, {
+});
+
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+//# API ROUTES
+app.get("/media", function(req, res){
+
+
+});
+
+app.get("/media/:id", function(req, res){
+
+});
+
+app.post("/media", function(req, res){
+
 });
 
 app.listen(PORT, function() {
