12fbfb92c7efa17eb57a71372fc8a6918dadd2b8
Steven <stevenesammon@hotmail.com>
2018-02-05 19:25:03 -0500

    fleshes out API routes

--- server.js
+++ server.js
@@ -20,40 +20,82 @@ const mongoDB_URI = process.env.MONGODB_URI || "mongodb://localhost/captivaDB";
 mongoose.connect(mongoDB_URI, {
 });
 
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //# API ROUTES
-app.get("/media", function(req, res){
+app.get("/media", function (req, res) {
 
-
-
+  db.Media.find({})
+    .then(function (dbMedia) {
+      res.json(dbMedia);
+    })
+    .catch(function (err) {
+     return res.json(err);
+    });
 
 });
 
-app.get("/media/:id", function(req, res){
+app.get("/media/:id", function (req, res) {
+
+  db.Media.findOne({ _id: req.params.id })
+    .then(function (dbMedia) {
+      res.json(dbMedia);
+    })
+    .catch(function (err) {
+      return res.json(err);
+    });
 
 });
 
-app.post("/media", function(req, res){
+app.post("/media", function (req, res) {
 
-  db.Media.create(req.body).then(function (dbMedia) {
-    console.log(dbMedia)
-}).catch(function (err) {
+  db.Media.create(req.body)
+    .then(function (dbMedia) {
+      console.log(dbMedia)
+    }).catch(function (err) {
+      return res.json(err);
+    });
+
+});
+
+app.get("/users", function (req, res) {
+
+  db.User.find({})
+    .then(function (dbUser) {
+      res.json(dbUser);
+    })
+    .catch(function (err) {
+      return res.json(err);
+    });
+
+});
+
+app.get("/users/:id", function (req, res) {
+
+  db.User.findOne({ _id: req.params._id })
+    .then(function (dbUser) {
+      res.json(dbUser);
+    })
+    .catch(function (err) {
+      return res.json(err);
+    });
+
+});
+
+
+app.post("/users", function (req, res) {
+
+  db.User.create(req.body).then(function (dbUser) {
+    console.log(dbUser);
+  }).catch(function (err) {
     return res.json(err);
-});
+  });
 
 });
 
-app.post("/users", function(req, res){
 
- 
-
-});
-
-app.
-
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
