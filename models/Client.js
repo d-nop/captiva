d533737e3c96a25e027e7f4ffbea52c56a3954e6
Steven <stevenesammon@hotmail.com>
2018-02-06 17:15:26 -0500

    adds authentication to model

--- models/Client.js
+++ models/Client.js
@@ -1,12 +1,48 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const SALT_WORK_FACTOR = 5;
 
 const ClientSchema = new mongoose.Schema({
-    name: { type: String, unique: true, required: true },
-    id: { type: String, required: true },
-    secret: { type: String, required: true },
-    userId: { type: String, required: true }
-  });
-  
-  const Client = mongoose.model('Client', ClientSchema);
+  name: { type: String, unique: true, required: true },
+  id: { type: String, required: true },
+  secret: { type: String, required: true },
+  userId: { type: String, required: true }
+});
 
-  module.exports = Client;
+ClientSchema.pre("save", function(next){
+
+  var user = this;
+
+   // only hash the password if it has been modified (or is new)
+   if (!user.isModified('password')) return next();
+
+   // generate a salt
+   bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+       if (err) return next(err);
+
+       // hash the password using our new salt
+       bcrypt.hash(user.password, salt, function(err, hash) {
+           if (err) return next(err);
+
+           // override the cleartext password with the hashed one
+           user.password = hash;
+           next();
+       });
+   });
+});
+
+
+
+
+function genSalt() {
+
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.hash(userPassword, salt, function (err, hash) {
+
+      userPassword = hash;
+    });
+  });
+}
+const Client = mongoose.model('Client', ClientSchema);
+
+module.exports = Client, genSalt;
