8ff7a52e0a3fceb833a064bd5f1063df76090459
Steven <stevenesammon@hotmail.com>
2018-02-07 10:25:35 -0500

    uncomments code, preparing for merge

--- server.js
+++ server.js
@@ -7,17 +7,15 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const db = require("./models");
 const cloudinary = require("cloudinary");
-//const cloudinaryKeys = require("./cloudinaryKeys");
+const cloudinaryKeys = require("./cloudinaryKeys");
 const bcrypt = require("bcrypt");
 
-
-
 //Configuring Cloudinary
-// cloudinary.config({
-//   cloud_name: cloudinaryKeys.cloud_name,
-//   api_key: cloudinaryKeys.cloudinary_api_key,
-//   api_secret: cloudinaryKeys.cloudinary_api_secret
-// });
+cloudinary.config({
+  cloud_name: cloudinaryKeys.cloud_name,
+  api_key: cloudinaryKeys.cloudinary_api_key,
+  api_secret: cloudinaryKeys.cloudinary_api_secret
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -33,9 +31,9 @@ const mongoDB_URI = process.env.MONGODB_URI || "mongodb://localhost/captivaDB";
 mongoose.connect(mongoDB_URI, {
 });
 
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 
 //# API ROUTES
@@ -105,11 +103,6 @@ app.get("/users/:id", function (req, res) {
   db.User.findOne({ _id: req.params.id })
     .then(function (dbUser) {
 
-      dbUser.comparePassword(dbUser.password, function (err, isMatch) {
-        if (err) throw err;
-        console.log('Password123:', isMatch); // -> Password123: true
-      })
-
     })
     .catch(function (err) {
       return res.json(err);
@@ -120,9 +113,7 @@ app.get("/users/:id", function (req, res) {
 
 app.post("/users", function (req, res) {
 
-  let userPassword;
-
-
+ 
 
   db.User.save(req.body).then(function (dbUser) {
     console.log(dbUser);
@@ -130,16 +121,7 @@ app.post("/users", function (req, res) {
     return res.json(err);
   });
 
-  db.User.update({ username: req.body.username }, { $set: { password: userPassword } })
-
-
-    .then(function (dbUser) {
-
-
-
-    })
-
-});
+ });
 
 app.listen(PORT, function () {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
