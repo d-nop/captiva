838f104c9f3385a0bda7984d173edf907adaa9ca
Ovie <you@example.com>
2018-02-13 20:47:46 -0500

    adds app.js and login.js changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,34 @@
-
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, } from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import {Grid, Row, Col} from 'react-boostrap';
-import {CSS, Grid, Row, Col} from 'react-bootstrap';
-import logo from './logo.svg';
-import './App.css';
+import { CSS, Grid, Row, Col } from "react-bootstrap";
+import logo from "./logo.svg";
+import "./App.css";
 // import Photo from './components/Pages/Photo';
 import Nav from "./components/Nav";
 import Login from "./Pages/Login";
-import Logo from "./components/Logo";
-import WebCapture from "./Pages/Webcapture";
-import Display from "./Pages/Display";
 
-class App extends Component{
-
-state ={
-  route:"/"
-}
+// import Logo from "./components/Logo";
+import WebCapture from "./Pages/WebCapture";
+import react from "react-router-dom";
+import MediaDisplay from "./Pages/MediaDisplay";
+import Capture from "./Pages/WebCapture"
 
 
 
 
+class App extends Component {
   render() {
     return (
-      <div className="container">
+
       <Router>
-          <Switch>
-              <Route exact path="/camera" component={WebCapture} />
-              <Route exact path="/" component= {Login} />
-              <Route exact path="/display" component= {Display} />
-          </Switch>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/Capture" component={Capture} />
+          <Route exact path="/MediaDisplay" component={MediaDisplay} />
+        </Switch>
       </Router>
-      </div>
-    )
+    );
   }
-
 }
-
-
 export default App;
--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,29 +1,33 @@
+
 import React from "react";
 import { Grid, Row, Col,  } from "react-bootstrap";
 import axios from "axios";
+import PawLogo from "../../utils/assets/images/PawLogo.png";
+import Background from "../../utils/assets/images/streetPhoto.png";
+import "./Login.css";
 
 import {
-	Navbar,
-	Form,
-	FormGroup,
-	FormControl,
-	Button,
-	ControlLabel
+  Navbar,
+  Form,
+  FormGroup,
+  FormControl,
+  Button,
+  ControlLabel
 
 } from "react-bootstrap";
 
 class Login extends React.Component {
-	state = {
-		username: "",
-		password: "",
-		submitId:"loginButton",
-		formId:"loginForm",
-		nameholder:"Your username",
-		passholder:"Your Password",
-		buttonVal: "Log In"
-	}
+  state = {
+    username: "",
+    password: "",
+    submitId:"loginButton",
+    formId:"loginForm",
+    nameholder:"Your username",
+    passholder:"Your Password",
+    buttonVal: "Log In"
+  }
 
-	handleSignup=event=>{
+  handleSignup=event=>{
         if(this.state.submitId==="loginButton"){
             this.setState({
                 nameholder:"Create a username",
@@ -35,7 +39,7 @@ class Login extends React.Component {
          }
             
         else{
-			this.setState({
+      this.setState({
             submitId:"loginButton",
             nameholder:"Your username",
             passholder:"Your Password",
@@ -47,100 +51,118 @@ class Login extends React.Component {
    }
 
 
-	handleChange = event => {
-		const { name, value } = event.target;
-		this.setState({ [name]: value });
-	}
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
 
 
-	handleSubmit = event => {
-		// Preventing the default behavior of the form submit (which is to refresh the page)
-		event.preventDefault();
-		console.log(event.target.id);
-		
+  handleSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    console.log(event.target.id);
+    
 
-		switch(event.target.id){
+    switch(event.target.id){
 
-			case "loginForm":
-				const loginCreds = {
-					username: this.state.username,
-					password:this.state.password
-				};
-				console.log(loginCreds);
-				axios.post ("/login", loginCreds)
-					.then(res => {
-						console.log(res);
-						const newObj ={username: res.data.username,
-										token: res.data.token};
-						localStorage.setItem("user", JSON.stringify(newObj));					
-					})
-					.catch(err=>console.log(err))
-				this.setState({
-					    username: "",
-					    password: ""
-				    })
-			break
-			case "signupForm":
-				const signupCreds = {
-					username: this.state.username,
-					password:this.state.password
-				};
-				console.log(signupCreds);
-				axios.post ("/register",signupCreds)
-					.then(res => {
-							console.log(res);
-							localStorage.setItem("username", res.username);
-							localStorage.setItem("token", res.token);					
-					})
-					.catch(err=>console.log(err))
-				this.setState({
-					    username: "",
-					    password: ""
-				    })
-				break
-		}
-	
-	}
+      case "loginForm":
+        const loginCreds = {
+          username: this.state.username,
+          password:this.state.password
+        };
+        console.log(loginCreds);
+        axios.post ("/login", loginCreds)
+          .then(res => {
+            console.log(res);
+            const newObj ={username: res.data.username,
+                    token: res.data.token};
+            localStorage.setItem("user", JSON.stringify(newObj));         
+          })
+          .catch(err=>console.log(err))
+        this.setState({
+              username: "",
+              password: ""
+            })
+      break
+      case "signupForm":
+        const signupCreds = {
+          username: this.state.username,
+          password:this.state.password
+        };
+        console.log(signupCreds);
+        axios.post ("/register",signupCreds)
+          .then(res => {
+              console.log(res);
+              localStorage.setItem("username", res.username);
+              localStorage.setItem("token", res.token);         
+          })
+          .catch(err=>console.log(err))
+        this.setState({
+              username: "",
+              password: ""
+            })
+        break
+    }
+  
+  }
 
-	render () {
-		return (
-			<div>
-			<Grid>
-			<Row>
-			<Col xs={12}>
+  render () {
+    return (
+       <div className="LoginPage">
+        <div>
+          <Grid>
+            <Row>
+              <Col xs={12}>
+                <Form
+                  inline
+                  onSubmit={this.handleSubmit}
+                  id="loginForm"
+                >
+                  <FormGroup controlId="formInlineName">
+                    {" "}
+                    <FormControl
+                      type="text"
+                      name="username"
+                      value={this.state.username}
+                      placeholder={this.state.nameholder}
+                      onChange={this.handleChange}
+                    />
+                  </FormGroup>{" "}
+                  <FormGroup controlId="formInlinePassword">
+                    {" "}
+                    <FormControl
+                      type="password"
+                      name="password"
+                      value={this.state.value}
+                      placeholder={this.state.passholder}
+                      onChange={this.handleChange}
+                    />
+                  </FormGroup>{" "}
+                  <Button
+                    type="submit"
+                    id={this.state.submitId}
+                  >
+                    {this.state.buttonVal}
+                  </Button>
+                  <a
+                    id="toggleButton"
+                    onClick={this.handleSignup}
+                  >
+                    {" "}
+                    Log In/Sign Up{" "}
+                  </a>
+                </Form>
+                <img src = {PawLogo} alt="logo" id="logo"/>
+                <Row center="xs">
+                          <h1 id="BrandName">Captiva</h1>
+                    </Row>
+              </Col>
+            </Row>
+          </Grid>
 
-				<Form inline onSubmit = {this.handleSubmit} id={this.state.formId}>
-					<FormGroup controlId="formInlineName">
-						{" "}
-						<FormControl
-							type="text"
-							name="username"
-							value={this.state.username}
-							placeholder={this.state.nameholder}
-							onChange={this.handleChange}
-						/>
-					</FormGroup>{" "}
-					<FormGroup controlId="formInlinePassword">
-						{" "}
-						<FormControl
-							type="password"
-							name="password"
-							value={this.state.value}
-							placeholder={this.state.passholder}
-							onChange={this.handleChange}
-						/>
-					</FormGroup>{" "}
-					<Button type="submit" id={this.state.submitId} >{this.state.buttonVal}
-					</Button>
-					<a  id= "toggleButton" onClick={this.handleSignup}>  Log In/Sign Up </a>
-				</Form>
-				
-				</Col>
-				</Row>
-				</Grid>
-				</div>
-		);
-	}
+        </div>
+      </div>
+    );
+  }
 }
-
 export default Login;
